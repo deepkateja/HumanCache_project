96c94c2586feb2c39a6e2f834fecc8353ff2e29f
deepkateja <kd.kateja@gmail.com>
2021-08-03 17:37:57 +0530

    Backend-Deployed

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.get('/',(req,res)=>{
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) //Booleans Ensures no warning in console
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .then(() => app.listen(process.env.PORT|| 5000, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
